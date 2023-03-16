4693e3741596c1a20326f4993a89704430c93ed7
Christian Donhofer <christian.donhofer@finabro.com>
2023-03-16 21:54:48 +0100

    Initial commit (Yippie!)
    * first parser implementation and tests
    * first expression reducer / calculation
    
    all in experimental state; API not yet defined
    this is currently just a PoC (with at least one known error)

---
+++ src/main/java/BigDecimalExp.java
@@ -0,0 +1,258 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+
+public class BigDecimalExp {
+    public static final int defaultScale = 5;
+    public static final RoundingMode defaultRoundingMode = RoundingMode.HALF_UP;
+
+    // operator symbols
+    public static final char ADD = '+';
+    public static final char SUBTRACT = '-';
+    public static final char MULTIPLY = '*';
+    public static final char DIVIDE = '/';
+    public static final char POW = '^';
+
+    // list of operators for easier handling - the order is vital!
+    static final List<Character> operators = List.of(POW, MULTIPLY, DIVIDE, ADD, SUBTRACT);
+
+    // map operators to methods
+    static final Map<Character, BinaryOperator<BigDecimal>> opToMethod = Map.of(
+            POW, BigDecimalExp::pow,
+            MULTIPLY, BigDecimalExp::multiply,
+            DIVIDE, BigDecimalExp::divide,
+            ADD, BigDecimalExp::add,
+            SUBTRACT, BigDecimalExp::subtract
+    );
+
+
+
+    RoundingMode roundingMode;
+    int scale;
+
+    public BigDecimalExp(int scale, RoundingMode roundingMode) {
+        this.roundingMode = roundingMode;
+        this.scale = scale;
+    }
+
+    public static BigDecimalExp create() {
+        // TODO should this perhaps set nothing at all? might cause trouble or bad code building the divisions
+        return new BigDecimalExp(defaultScale, defaultRoundingMode);
+    }
+
+    public static BigDecimalExp with(int scale, RoundingMode roundingMode) {
+        return new BigDecimalExp(scale, roundingMode);
+    }
+
+    public BigDecimal eval(String exp, Map.Entry<String, BigDecimal>... params) {
+        Map<String, BigDecimal> paramsMap = new HashMap<>();
+        for(Map.Entry<String, BigDecimal> param : params) {
+            paramsMap.put(param.getKey(), param.getValue());
+        }
+
+        return eval(exp, paramsMap);
+    }
+
+    public BigDecimal eval(String exp, Map<String, BigDecimal> params) throws ArithmeticException {
+        /*
+        validate input
+         */
+
+        // no. of parentheses
+        int numOpeningParentheses = countOccurrences(exp, '(');
+        int numClosingParentheses = countOccurrences(exp, ')');
+        if(numOpeningParentheses != numClosingParentheses) {
+            throw new ArithmeticException("Different no. of opening and closing parentheses");
+        }
+
+        // TODO disallow multiline strings
+
+        // remove spaces and trim opening/closing parentheses
+        exp = exp.strip().replaceAll("\\s", "");
+        if(exp.charAt(0) == '(' && exp.charAt(exp.length()-1) == ')') {
+            exp = exp.substring(1, exp.length()-1);
+        }
+
+        char[] chars = exp.toCharArray();
+
+        /*
+         * parse, each opening parenthesis creates a recursive call of this method
+         */
+        // collect operations as a linked list with a dummy start node
+        Node startNode = new Node(null, ' ');
+        Node node = startNode;
+
+        // keep track of operators to allow for an efficient in-order application once parsing is done
+        Map<Character, List<Node>> nodesPerOp = new HashMap<>();
+        operators.forEach(op -> nodesPerOp.put(op, new ArrayList<>()));
+
+        int start = 0;
+        for(int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            boolean isOp = isOperator(c);
+            boolean isEnd = i == chars.length-1;
+
+            // within a term / expression: continue
+            if(!isEnd && c != '(' && !isOp) {
+                continue;
+            }
+
+            // case where operator is the first sign encountered, which happens after a sub-expression has been parsed
+            if(isOp && start == i) {
+                if(node == startNode){
+                    throw new ArithmeticException(String.format("An expression must not start with an operator: %s", exp));
+                }
+                node.op = c;
+
+                // continue at next char
+                start = i + 1;
+            }else if(isOp || isEnd) {
+                // get the term that ends here / at the last pos
+                int expLastChar = isEnd ? i : i - 1;
+                String currentTerm = new String(Arrays.copyOfRange(chars, start, expLastChar + 1)).trim();
+                // check if term is a parameter and if so, get it; else, treat term as value and create BigDecimal
+                BigDecimal val = Optional
+                        .ofNullable(params.get(currentTerm))
+                        .orElseGet(() -> new BigDecimal(currentTerm));
+
+                node.next = new Node(val, isEnd ? null : c);
+                node.next.prev = node;
+                node = node.next;
+            } else { // start of sub-expression
+
+                // if symbol before this parenthesis was neither an operator, nor start of expression, nor another parenthesis
+                // this is an implicit multiplication, which isn't supported by now
+                // when(if ever) implementing this, take into account all cases: terms from sub-expressions will be inside
+                // a node already, which is simply missing the operator
+                // else (i.e. the node before has an operator), fetch the ongoing term and add a multiplication node
+                if(i != 0 && !isOperator(chars[i-1]) && chars[i-1] != '(') {
+                    throw new ArithmeticException("Implicit multiplication is currently not supported. Please use the multiplication operator");
+                }
+
+
+                // find matching closing parenthesis
+                int pCnt = 1;
+                int pEnd = i;
+                while(pCnt > 0) {
+                    pEnd++;
+                    if(chars[pEnd] == '(') pCnt++;
+                    if(chars[pEnd] == ')') pCnt--;
+                }
+                // parse sub-expression
+                // but ignore empty parentheses: ()
+                if(pEnd - i > 1) {
+                    // add sub expression and continue
+                    BigDecimal subExp = eval(new String(Arrays.copyOfRange(chars, i+1, pEnd)), params);
+                    node.next = new Node(subExp, null);
+                    node.next.prev = node;
+                    node = node.next;
+                }
+                i = pEnd;
+            }
+
+            if(isOp) {
+                //add to occ. map
+                nodesPerOp.get(c).add(node);
+            }
+
+            // continue at next char
+            start = i + 1;
+
+        }
+        /*
+         * possible problems
+         * operator in the last position!?
+         */
+
+        System.out.println("----------------------------------");
+
+        System.out.println("found these terms: ");
+        node = startNode.next;
+        while(node != null) {
+            System.out.println(" "+node.val.toString()+" "+Optional.ofNullable(node.op).orElse(' '));
+            node = node.next;
+        }
+//
+//        System.out.println("----------------------------------");
+//
+//        System.out.println("found these operator nodes: ");
+//        operators.forEach(op -> {
+//            System.out.println("Operator "+op+"("+nodesPerOp.get(op).size()+")");
+//            System.out.println(nodesPerOp.get(op).stream().map(n -> ""+n.val+n.op+n.next.val).collect(Collectors.joining(";")));
+//        });
+
+        // apply operations
+        for(char op : operators) {
+            BinaryOperator<BigDecimal> operation = opToMethod.get(op);
+            for(Node n : nodesPerOp.get(op)) {
+                // keep refs of adjacent nodes
+                Node left = n.prev;
+                Node secondOperand = n.next;
+
+                // write result to right operand, as it possible contains an operation with another node - left one is unlinked
+                secondOperand.val = operation.apply(n.val, secondOperand.val);
+
+                // unlink processed node
+                secondOperand.prev = left;
+                left.next = secondOperand;
+            }
+        }
+        System.out.println("----------------------------------");
+
+        System.out.println("final terms: ");
+        node = startNode.next;
+        while(node != null) {
+            System.out.println(" "+node.val.toString()+" "+Optional.ofNullable(node.op).orElse(' '));
+            node = node.next;
+        }
+
+        System.out.println("----------------------------------");
+        return startNode.next.val;
+    }
+
+    // todo these could go into a separate class
+    // do I even need them, or could I use simple lambdas calling the bigdecimal instance/member function instead? I think I could!
+    static BigDecimal pow(BigDecimal a, BigDecimal b) {
+        return a.pow(b.intValue());
+    }
+
+
+    static BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        return a.multiply(b);
+    }
+
+    static BigDecimal divide(BigDecimal a, BigDecimal b) {
+        return a.divide(b);
+    }
+
+    static BigDecimal add(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+
+    static BigDecimal subtract(BigDecimal a, BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    // utility methods
+    private boolean isOperator(Character c) {
+        return operators.contains(c);
+    }
+
+    private int countOccurrences(String haystack, char needle) {
+        return haystack.length() - haystack.replace(""+needle, "").length();
+    }
+
+    private static class Node {
+        BigDecimal val;
+        Character op;
+        Node prev;
+        Node next;
+
+        Node(BigDecimal val, Character op) {
+            this.val = val;
+            this.op = op;
+        }
+    }
+}
---
+++ src/test/java/ExpressionParserTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class ExpressionParserTest {
+
+    /**
+     * tests the parsing process only, i.e. if it succeeds or fails as expected
+     * @param expression
+     * @param params
+     */
+    @ParameterizedTest
+    @MethodSource("getStandardExpressions")
+    public void testStandardCases(String expression, Map<String, BigDecimal> params, boolean shouldSucceed){
+        try {
+            BigDecimal parsedResult = BigDecimalExp.create().eval(expression, params);
+        } catch (Exception e) {
+            if(shouldSucceed) {
+               throw e;
+            } else {
+                System.out.println("exception: "+e.getCause());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Stream<Arguments> getStandardExpressions() {
+        return Stream.of(
+                Arguments.of("a ^ 2 *((c/10)+b*c+a)", Map.of("a", new BigDecimal("0.014000"), "b", new BigDecimal("2"), "c", new BigDecimal("13.73")), true),
+                Arguments.of("0.014000 ^ 2 *((13.73/10)+2*13.73+0.014000)", Map.of(), true),
+                Arguments.of("0.014000 ^ 2*() *((13.73/10)+2*13.73+0.014000)", Map.of(), true), // ()
+                Arguments.of("a ^ 2 *((c/10)+b*c+a)", Map.of("a", new BigDecimal("0.014000"), "b", new BigDecimal("2")), false), // missing parameter
+                Arguments.of("0,014000 ^ 2 *((13.73/10)+2*13.73+0.014000)", Map.of(), false), // wrong decimal separator
+                Arguments.of("0.014000 | 2 *((13.73/10)+2*13.73+0.014000)", Map.of(), false), // unknown operator
+                Arguments.of("0.014000 ^^ 2 *((13.73/10)+2*13.73+0.014000)", Map.of(), false), // double operator
+                Arguments.of("0.014000 ^ 2 *((13.73/10)+2*13.73+0.014000", Map.of(), false), // missing parenthesis
+                Arguments.of("0.014000 ^ 2 ((13.73/10)+2*13.73+0.014000)", Map.of(), false) // implicit multiplication
+        );
+    }
+}
