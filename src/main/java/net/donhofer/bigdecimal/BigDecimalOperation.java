9eae71e79d28b25e0b34d85bbff8f0b5803774d2
Christian Donhofer <christian.donhofer@finabro.com>
2023-05-07 18:07:09 +0200

    preparing for release 1.0
    missing javadoc, added doc and sources

--- src/main/java/net/donhofer/bigdecimal/BigDecimalOperation.java
+++ src/main/java/net/donhofer/bigdecimal/BigDecimalOperation.java
@@ -2,7 +2,20 @@ package net.donhofer.bigdecimal;
 
 import java.math.RoundingMode;
 
+/**
+ * functional interface describing a big decimal operation
+ * @param <BigDecimal> operand typ is fixed to BigDecimal
+ * @param <T> return type
+ */
 @FunctionalInterface
 public interface BigDecimalOperation<BigDecimal, T> {
+    /**
+     * apply the operation
+     * @param a left operand
+     * @param b right operand
+     * @param scale scale for division operations
+     * @param roundingMode rounding mode for division operations
+     * @return the result of type T
+     */
     T apply(BigDecimal a, BigDecimal b, int scale, RoundingMode roundingMode);
 }
